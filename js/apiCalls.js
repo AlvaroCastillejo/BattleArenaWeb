0cc20042b579e27f2a51b50b9f8a37a8e2174019
ferran <ferran.gimeno10@gmail.com>
2021-01-01 21:14:25 +0100

    fix: Console.
    Fixed an issue that showed a message every 2 seconds.

--- js/apiCalls.js
+++ js/apiCalls.js
@@ -19,8 +19,10 @@ async function fetchSpawn(name) {
                 player.name = name;
                 fetchPlayer(player.id);
                 intervalTimer = setInterval(myTimer, 2000);
+                showMessageConsole("Player created succesfully.")
             })
         })
+
         /*OLD SYSTEM OPERATIONAL
         .then(response => response.json())
         .then(data => {
@@ -56,7 +58,6 @@ function fetchPlayer(token) {
                 document.getElementById("player_avatar").setAttribute("style", 'grid-area: player_pic;    margin-left: 20px;    background-image: url("assets/avatars/my_character-'+ player.image
                     +'.png");    background-repeat: no-repeat;    background-size: 220%;    background-position: -90px;    background-position-y: -40px;');         //document.getElementById("ambient").play();
                 updateHPBar();
-                showMessageConsole("Player created succesfully.")
             }).then(() => {
                 fetchMap(player.id);
                 //fetchMap(token);
@@ -125,18 +126,18 @@ function updateHPBar() {
     console.log("lost hp: " + lost_hp);
     if(isNaN(lost_hp)) return;
     let percentageToTake = (lost_hp*100)/full_hp;
-    if(lost_hp !== 0){
+    if(lost_hp >= 0 ){
         moveBar(percentageToTake);
     }
 }
-
+let i = 0;
 function moveBar(n) {
-    var i = 0;
     if (i === 0) {
         i = 1;
-        var elem = document.getElementById("myBar");
-        var width = last_hp;
-        var id = setInterval(frame, 10);
+        let elem = document.getElementById("myBar");
+        let width = last_hp;
+        console.log(last_hp);
+        let id = setInterval(frame, 10);
         function frame() {
             if (width <= n) {
                 clearInterval(id);
